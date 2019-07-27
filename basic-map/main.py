78e240f4809d32247f8404272f7393a7ecb9b64a
Bastien Pesenti <bpesenti@aligntech.com>
2019-07-26 17:50:04 -0700

    Terrain Cube

--- basic-map/main.py
+++ basic-map/main.py
@@ -26,7 +26,8 @@ import os
 
 base = ShowBase()
 base.disableMouse()
-base.camera.setPos(0, -10, 0)
+base.camera.setPos(10, -20, 10)
+base.camera.lookAt(0, 0, 0)
 
 title = OnscreenText(text="Panda3D: Tutorial - Making a Cube Procedurally",
                      style=1, fg=(1, 1, 1, 1), pos=(-0.1, 0.1), scale=.07,
@@ -51,86 +52,84 @@ def normalized(*args):
     myVec.normalize()
     return myVec
 
-# helper function to make a square given the Lower-Left-Hand and
-# Upper-Right-Hand corners
 
-def makeSquare(x1, y1, z1, x2, y2, z2):
+def makeTerrainCube():
+    
     format = GeomVertexFormat.getV3n3cpt2()
     vdata = GeomVertexData('square', format, Geom.UHDynamic)
-
     vertex = GeomVertexWriter(vdata, 'vertex')
     normal = GeomVertexWriter(vdata, 'normal')
     color = GeomVertexWriter(vdata, 'color')
     texcoord = GeomVertexWriter(vdata, 'texcoord')
-
-    # make sure we draw the sqaure in the right plane
-    if x1 != x2:
-        vertex.addData3(x1, y1, z1)
-        vertex.addData3(x2, y1, z1)
-        vertex.addData3(x2, y2, z2)
-        vertex.addData3(x1, y2, z2)
-
-        normal.addData3(normalized(2 * x1 - 1, 2 * y1 - 1, 2 * z1 - 1))
-        normal.addData3(normalized(2 * x2 - 1, 2 * y1 - 1, 2 * z1 - 1))
-        normal.addData3(normalized(2 * x2 - 1, 2 * y2 - 1, 2 * z2 - 1))
-        normal.addData3(normalized(2 * x1 - 1, 2 * y2 - 1, 2 * z2 - 1))
-
-    else:
-        vertex.addData3(x1, y1, z1)
-        vertex.addData3(x2, y2, z1)
-        vertex.addData3(x2, y2, z2)
-        vertex.addData3(x1, y1, z2)
-
-        normal.addData3(normalized(2 * x1 - 1, 2 * y1 - 1, 2 * z1 - 1))
-        normal.addData3(normalized(2 * x2 - 1, 2 * y2 - 1, 2 * z1 - 1))
-        normal.addData3(normalized(2 * x2 - 1, 2 * y2 - 1, 2 * z2 - 1))
-        normal.addData3(normalized(2 * x1 - 1, 2 * y1 - 1, 2 * z2 - 1))
-
-    # adding different colors to the vertex for visibility
-    color.addData4f(1.0, 0.0, 0.0, 1.0)
-    color.addData4f(0.0, 1.0, 0.0, 1.0)
-    color.addData4f(0.0, 0.0, 1.0, 1.0)
-    color.addData4f(1.0, 0.0, 1.0, 1.0)
-
-    texcoord.addData2f(0.0, 1.0)
-    texcoord.addData2f(0.0, 0.0)
-    texcoord.addData2f(1.0, 0.0)
-    texcoord.addData2f(1.0, 1.0)
-
-    # Quads aren't directly supported by the Geom interface
-    # you might be interested in the CardMaker class if you are
-    # interested in rectangle though
+    
     tris = GeomTriangles(Geom.UHDynamic)
-    tris.addVertices(0, 1, 3)
-    tris.addVertices(1, 2, 3)
 
-    square = Geom(vdata)
-    square.addPrimitive(tris)
-    return square
+    # Top Plane
+    vertex.add_data3(-1,-1, 1)
+    vertex.add_data3( 1,-1, 1)
+    vertex.add_data3( 1, 1, 1)
+    vertex.add_data3(-1, 1, 1)
+    for x in range(4):
+        color.addData4f(0.0, 0.8, 0.4, 1.0)
+        normal.addData3(0,0,1)
+    tris.addVertices(0, 1, 2)
+    tris.addVertices(0, 2, 3)
 
-# Note: it isn't particularly efficient to make every face as a separate Geom.
-# instead, it would be better to create one Geom holding all of the faces.
-square0 = makeSquare(-1, -1, -1, 1, -1, 1)
-square1 = makeSquare(-1, 1, -1, 1, 1, 1)
-square2 = makeSquare(-1, 1, 1, 1, -1, 1)
-square3 = makeSquare(-1, 1, -1, 1, -1, -1)
-square4 = makeSquare(-1, -1, -1, -1, 1, 1)
-square5 = makeSquare(1, -1, -1, 1, 1, 1)
-snode = GeomNode('square')
-snode.addGeom(square0)
-snode.addGeom(square1)
-snode.addGeom(square2)
-snode.addGeom(square3)
-snode.addGeom(square4)
-snode.addGeom(square5)
+    #Side Planes
+    vertex.add_data3(1,-1,-1)
+    vertex.add_data3(1, 1,-1)
+    vertex.add_data3(1, 1, 1)
+    vertex.add_data3(1,-1, 1)
 
+    vertex.add_data3( 1, 1,-1)
+    vertex.add_data3(-1, 1,-1)
+    vertex.add_data3(-1, 1, 1)
+    vertex.add_data3( 1, 1, 1)
+
+    vertex.add_data3(-1, 1,-1)
+    vertex.add_data3(-1,-1,-1)
+    vertex.add_data3(-1,-1, 1)
+    vertex.add_data3(-1, 1, 1)
+
+    vertex.add_data3(-1,-1,-1)
+    vertex.add_data3( 1,-1,-1)
+    vertex.add_data3( 1,-1, 1)
+    vertex.add_data3(-1,-1, 1)
+
+    for x in range(4):
+        normal.addData3(1,0,0)
+    for x in range(4):
+        normal.addData3(0,1,0)
+    for x in range(4):
+        normal.addData3(-1,0,0)
+    for x in range(4):
+        normal.addData3(0,-1,0)
+
+    for x in range (4):
+        tris.addVertices(4*(x+1), 4*(x+1)+1, 4*(x+1)+2)
+        tris.addVertices(4*(x+1), 4*(x+1)+2, 4*(x+1)+3)
+
+    for x in range(4):
+        color.addData4f(0.6, 0.2, 0.2, 1.0)
+        color.addData4f(0.6, 0.2, 0.2, 1.0)
+        color.addData4f(0.0, 0.8, 0.4, 1.0)
+        color.addData4f(0.0, 0.8, 0.4, 1.0)    
+
+    for x in range(5):
+        texcoord.addData2f(0.0, 0.0)
+        texcoord.addData2f(1.0, 0.0)
+        texcoord.addData2f(0.0, 1.0)
+        texcoord.addData2f(1.0, 1.0)
+
+    terrainCube = Geom(vdata)
+    terrainCube.addPrimitive(tris)
+    return terrainCube
+
+terrainCube0 = makeTerrainCube()
+snode = GeomNode('terrainCube')
+snode.addGeom(terrainCube0)
 cube = render.attachNewNode(snode)
-cube.hprInterval(1.5, (360, 360, 360)).loop()
-
-# OpenGl by default only draws "front faces" (polygons whose vertices are
-# specified CCW).
 cube.setTwoSided(True)
-
 
 class MyTapper(DirectObject):
 
