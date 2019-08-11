73c1528a0a19132101ebff6d178ac438c932d9a8
Bastien Pesenti <bpesenti@yahoo.fr>
2019-08-11 08:56:09 -0700

    reorg folders

---
+++ basic-map/tree.py
@@ -0,0 +1,17 @@
+#########################################################################################
+# LightWorld
+# tree.py
+# Author: Bastien Pesenti (bpesenti@yahoo.fr)
+# Date: 7/26/2019
+
+#########################################################################################
+# Class TreeInfo, containing all specification for the tree
+class TreeInfor:
+    def __init__(self):
+        self.height = 5.0
+        pass
+
+class TreeMesher:
+    def __init__(self):
+        pass
+
